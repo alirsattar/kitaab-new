e7a94df96d1c4cde49ec81f580a4fc9181b920a5
Ali Sattar <alirsattar@gmail.com>
2020-07-25 12:00:15 -0400

    Saving before switching to TS project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,30 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Set up DB connection
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+);
+
+const testDbConnection = async ()=> {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+testDbConnection();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
